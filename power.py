0ac040caa495d78aef840abf8f652eebf4cb20da
Abdullah Sajjad <abdullah.sajjad@ocloudsolutions.net>
2025-01-13 18:14:34 +0500

    Power Function using Recursion

---
+++ power.py
@@ -0,0 +1,21 @@
+def helper(num, pow, res):
+    if pow == 0:
+        return 1
+    if pow == 1:
+        return res
+    return helper(num, pow - 1, res * num)
+
+def power_func(num, pow):
+    flag = pow >= 0
+    pow = abs(pow)
+    if flag:
+        return helper(num, pow, num)
+    return 1 / helper(num, pow, num)
+
+print(power_func(4,0))
+print(power_func(4,1))
+print(power_func(4,-2))
+print(power_func(0,0))
+print(power_func(0,2))
+print(power_func(-4,2))
+print(power_func(-4,-2))
